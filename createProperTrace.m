c5e17bbe57e4fb80469c6df6135fca2e935af28f
Christoffer Oxelmark Krook <christoffer@oxelmarkkrook.se>
2025-12-02 21:45:36 +0100

    Updated trace simulation to use efficient matlab vector operations. Total LSF retrieval time per iteration halved on my local machine.

--- createProperTrace.m
+++ createProperTrace.m
@@ -1,10 +1,18 @@
-function trace = createProperTrace(x, F, D)
+function trace = createProperTrace3(x, F, D)
 %   Standard fourier-domain delay of temporal signal.
-N = length(D);
-trace = zeros(length(F), N);
+N = length(x);
+fieldFFT_col = fft(x(:)); % Column vector
+D_row = D(:)';                 % Row vector
+F_col = F(:);            % Column vector
 
-for i = 1:length(D)
-    P = ifft( fft(x).*exp(1i*2*pi*D(i)*F) );
-    trace(:, i) = fftshift( abs(fft(P.*x)/N) ).^2;
-end
+phase_shifts_matrix = exp(1i * 2 * pi * (F_col * D_row)); 
+
+fieldFFT_matrix = repmat(fieldFFT_col, 1, length(D_row));
+
+P_matrix = ifft(fieldFFT_matrix .* phase_shifts_matrix);
+
+field_col = x(:);
+P_times_field = P_matrix .* field_col; % field_col expands across columns
+
+trace = fftshift(abs(fft(P_times_field)/N).^2, 1);
 end
